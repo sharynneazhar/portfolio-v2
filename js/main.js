61334173ac269f14584383f47dd144147491fb5d
Sharynne Azhar <Sharynne.Azhar@cerner.com>
2017-07-05 22:41:08 -0500

    large update haul

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-
+  
   /* Scroll hire me button to contact page */
-  $('.hire-me').click(function() {
+  $('.about-me').click(function() {
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 1000);
@@ -9,60 +9,12 @@ $(document).ready(function() {
   });
 
   /* For Bootstrap current state on portfolio sorting */
-
   $('ul.nav-pills li a').click(function(e) {
     $('ul.nav-pills li.active').removeClass('active')
     $(this).parent('li').addClass('active')
   })
-
-  /* portfolio mixitup */
-
-  $(window).load(function() {
-    var $container = $('.grid-wrapper');
-    $container.isotope({
-      filter: '*',
-      animationOptions: {
-        duration: 750,
-        easing: 'linear',
-        queue: false
-      }
-    });
-
-    $('.grid-controls li a').click(function() {
-      $('.grid-controls .current').removeClass('current');
-      $(this).addClass('current');
-
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false
-        }
-      });
-      return false;
-    });
-  });
-
-
-  /* Magnific Popup */
-  $('.grid-wrapper').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery: {
-      enabled: true
-    }
-  });
-
-
-  /* Sticky menu */
-  $(".navbar").sticky({
-    topSpacing: 0
-  });
-
-
-  /* Scroll spy and scroll filter */
+  
+  /* Scrollspy and scroll filter */
   $('#main-menu').onePageNav({
     currentClass: "active",
     changeHash: false,
@@ -71,45 +23,5 @@ $(document).ready(function() {
     filter: ":not(#external)",
     easing: "swing"
   });
-
-  /* Charts*/
-
-  $('.chart').waypoint(function() {
-    $(this).easyPieChart({
-      barColor: '#55a996',
-      size: '150',
-      easing: 'easeOutBounce',
-      onStep: function(from, to, percent) {
-        $(this.el).find('.percent').text(Math.round(percent));
-      }
-    });
-  }, {
-    triggerOnce: true,
-    offset: 'bottom-in-view'
-  });
-
-
-  /* VEGAS Home Slider */
-
-  $('#slide-welcome').vegas({
-    slides: [
-      { src: 'img/slider/01.jpg' },
-      // { src: 'img/slider/02.jpg' },
-      { src: 'img/slider/03.jpg' },
-      // { src: 'img/slider/04.jpg' },
-      { src: 'img/slider/05.jpg' }
-    ],
-    overlay: true,
-  });
-
-
-  $("#vegas-next").click(function() {
-    $('#slide-welcome').vegas('next');
-  });
-  $("#vegas-prev").click(function() {
-    $('#slide-welcome').vegas('previous');
-  });
-
-
-
+  
 });
